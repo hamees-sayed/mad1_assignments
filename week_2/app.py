af19fdc972fee636724fdbcd95fbfb1ba834559b
Hamees Sayed <98336593+hamees-sayed@users.noreply.github.com>
2023-10-15 18:38:59 +0000

    week 3 assignment

--- week_2/app.py
+++ week_2/app.py
@@ -1,122 +1,120 @@
 import sys
-import csv
 import matplotlib.pyplot as plt
 from jinja2 import Template
-from pyhtml import *
 
 
-# storing csv data in local data structure
-s_id = []
-c_id = []
-marks = []
 data = []
-
-with open('data.csv', mode='r') as file:
-    csvFile = csv.reader(file)
-    for lines in csvFile:
-        s_id.append(lines[0].strip())
-        c_id.append(lines[1].strip())
-        marks.append(lines[2].strip())
-        data.append({"student_id": lines[0], "course_id": lines[1], "marks": lines[2]})
-
-data = data[1:]
-s_id = s_id[1:]
-c_id = c_id[1:]
-marks = marks[1:]
-for d in data:
-    for key, value in d.items():
-        d[key] = value.strip()
+with open('data.csv', 'r', encoding="utf-8") as data_file:
+    data_file.readline()
+    for i in data_file.readlines():
+        data.append(list(map(int, i.strip('\n').split(','))))
 
 
-def avg_marks(data, id):
-    total_marks = 0
-    count = 0
-    for elem in data:
-        if elem['course_id'] == id:
-            total_marks += int(elem['marks'])
-            count += 1
-    return total_marks / count if count > 0 else 0
+def error_page():
+    err_page = """
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        </head>
+        <body>
+            <h1>Wrong Inputs</h1>
+            <p>Something went wrong</p>
+        </body>
+        </html>"""
 
-def max_marks(data, id):
-    max_marks = 0
-    for elem in data:
-        if elem['course_id'] == id:
-            marks = int(elem['marks'])
-            if marks > max_marks:
-                max_marks = marks
-    return max_marks
+    with open('output.html', 'w', encoding="utf-8") as err_output:
+        err_output.write(err_page)
 
+def student_page(s_id):
+    if s_id not in [i[0] for i in data]:
+        error_page()
+        return
 
-# templating the file to generate
-STUDENT = """<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>
-    <h1>Student Details</h1>
-    <table border=1>
-        <tr>
-            <th>Student id</th>
-            <th>Course id</th>
-            <th>Marks</th>
-        </tr>
-        {% for elem in data if elem['student_id'] == id %}
-        <tr>
-            <td>{{elem['student_id']}}</td>
-            <td>{{elem['course_id']}}</td>
-            <td>{{elem['marks']}}</th>
-        </tr>
-        {% endfor %}
-    </table>
-</body>
-</html>
-"""
+    student = """<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        </head>
+        <body>
+            <h1>Student Details</h1>
+            <table border=1>
+                <tr>
+                    <th>Student ID</th>
+                    <th>Course ID</th>
+                    <th>Marks</th>
+                </tr>
+                {% for i in s_data %}
+                <tr>
+                    <td>{{ i[0] }}</td>
+                    <td>{{ i[1] }}</td>
+                    <td>{{ i[2] }}</td>
+                </tr>
+                {% endfor %}
+                <tr>
+                    <td colspan=2>Total Marks</td>
+                    <td>{{ tot_marks }}</td>
+                </tr>
+            </table>
+        </body>
+        </html>"""
 
+    s_data = []
+    tot_marks = 0
+    for s in data:
+        if s[0] == s_id:
+            s_data.append(s)
+            tot_marks += s[2]
 
-COURSE = html(
-    head(
-        meta(charset="UTF-8"),
-        meta(name="viewport", content="width=device-width, initial-scale=1.0"),
-        title("Course Details")
-    ),
-    body(
-        h1("Course Details"),
-        table(border=1)(
-            tr(
-                th("Average Marks"),
-                th("Maximum Marks")
-            ),
-            tr(
-                td(avg_marks(data, id)),
-                td(max_marks(data, id))
-            )
-        )
-    )
-)
+    with open('output.html', 'w', encoding="utf-8") as st_output:
+        st_output.write(Template(student).render(s_data=s_data, tot_marks=tot_marks))
 
+def course_page(c_id):
+    if c_id not in[i[1] for i in data]:
+        error_page()
+        return
 
-def main(data, id, page):
-    f = open("output.html", "w")
-    if page == STUDENT:
-        template = Template(page)
-        f.write(template.render(data=data, id=id))
+    course = """<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        </head>
+        <body>
+            <h1>Course Details</h1>
+            <table border=1>
+                <tr>
+                    <th>Average Marks</th>
+                    <th>Maximum Marks</th>
+                </tr>
+                <tr>
+                    <td>{{ avg_marks }}</td>
+                    <td>{{ max_marks }}</td>
+                </tr>
+            </table>
+            <img src="hist_of_marks.png" alt="histogram of marks">
+        </body>
+        </html>"""
+
+    c_marks = []
+    for c in data:
+        if c_id == c[1]:
+            c_marks.append(c[2])
+
+    plt.hist(c_marks)
+    plt.xlabel('Marks')
+    plt.ylabel('Frequency')
+    plt.savefig('hist_of_marks.png')
+
+    with open('output.html', 'w', encoding="utf-8") as c_output:
+        c_output.write(Template(course).render(avg_marks=sum(c_marks)/len(c_marks), max_marks=max(c_marks)))
+
+if __name__ == '__main__':
+    if sys.argv[1] == '-s':
+        student_page(int(sys.argv[2]))
+    elif sys.argv[1] == '-c':
+        course_page(int(sys.argv[2]))
     else:
-        f.write(COURSE.render(data=data, id=id))
-    f.close()
-
-
-def param(arg1, arg2):
-    if (arg1 == '-c') and (arg2 in c_id):
-        main(data, arg2, COURSE)
-    elif (arg1 == '-s') and (arg2 in s_id):
-        main(data, arg2, STUDENT)
-    else:
-        print('Invalid input')
-
-
-if len(sys.argv) != 3:
-    print("Usage: python script.py <option> <id>")
-else:
-    param(sys.argv[1], sys.argv[2])
+        error_page()
