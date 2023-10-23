3fcdf63ef7bdb330059d191ee26f29c733ff31e9
Hamees Sayed <98336593+hamees-sayed@users.noreply.github.com>
2023-10-23 12:44:03 +0000

    folder restructure

--- week_3/app.py
+++ week_3/app.py
@@ -1,8 +1,7 @@
-import os
-from flask import Flask, render_template, request
+import sys
 import matplotlib.pyplot as plt
+from jinja2 import Template
 
-app = Flask(__name__)
 
 data = []
 with open('data.csv', 'r', encoding="utf-8") as data_file:
@@ -10,12 +9,57 @@ with open('data.csv', 'r', encoding="utf-8") as data_file:
     for i in data_file.readlines():
         data.append(list(map(int, i.strip('\n').split(','))))
 
+
 def error_page():
-    return render_template("error.html")
+    err_page = """
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        </head>
+        <body>
+            <h1>Wrong Inputs</h1>
+            <p>Something went wrong</p>
+        </body>
+        </html>"""
+
+    with open('output.html', 'w', encoding="utf-8") as err_output:
+        err_output.write(err_page)
 
 def student_page(s_id):
     if s_id not in [i[0] for i in data]:
-        return error_page()
+        error_page()
+        return
+
+    student = """<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        </head>
+        <body>
+            <h1>Student Details</h1>
+            <table border=1>
+                <tr>
+                    <th>Student ID</th>
+                    <th>Course ID</th>
+                    <th>Marks</th>
+                </tr>
+                {% for i in s_data %}
+                <tr>
+                    <td>{{ i[0] }}</td>
+                    <td>{{ i[1] }}</td>
+                    <td>{{ i[2] }}</td>
+                </tr>
+                {% endfor %}
+                <tr>
+                    <td colspan=2>Total Marks</td>
+                    <td>{{ tot_marks }}</td>
+                </tr>
+            </table>
+        </body>
+        </html>"""
 
     s_data = []
     tot_marks = 0
@@ -24,51 +68,53 @@ def student_page(s_id):
             s_data.append(s)
             tot_marks += s[2]
 
-    return render_template("student.html", s_data=s_data, tot_marks=tot_marks)
+    with open('output.html', 'w', encoding="utf-8") as st_output:
+        st_output.write(Template(student).render(s_data=s_data, tot_marks=tot_marks))
 
 def course_page(c_id):
     if c_id not in[i[1] for i in data]:
-        return error_page()
+        error_page()
+        return
+
+    course = """<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        </head>
+        <body>
+            <h1>Course Details</h1>
+            <table border=1>
+                <tr>
+                    <th>Average Marks</th>
+                    <th>Maximum Marks</th>
+                </tr>
+                <tr>
+                    <td>{{ avg_marks }}</td>
+                    <td>{{ max_marks }}</td>
+                </tr>
+            </table>
+            <img src="hist_of_marks.png" alt="histogram of marks">
+        </body>
+        </html>"""
 
     c_marks = []
     for c in data:
         if c_id == c[1]:
             c_marks.append(c[2])
 
-    path = "./static"
-    if not os.path.exists(path):
-        os.mkdir(path)
     plt.hist(c_marks)
     plt.xlabel('Marks')
     plt.ylabel('Frequency')
-    plt.savefig('./static/hist_of_marks.png')
+    plt.savefig('hist_of_marks.png')
 
-    return render_template("course.html", avg_marks=sum(c_marks)/len(c_marks), max_marks=max(c_marks))
-
-@app.route('/', methods=["GET", "POST"])
-def main():
-    if request.method == "GET":
-        return render_template('index.html')
-    elif request.method == "POST":
-        id_category = request.form.get("ID")
-        id_value = request.form.get("id_value")
-
-        if not id_category or not id_value:
-            return error_page()
-
-        try:
-            id_value = int(id_value)
-        except ValueError:
-            return error_page()
-
-        if id_category == "student_id":
-            return student_page(id_value)
-        elif id_category == "course_id":
-            return course_page(id_value)
-        else:
-            return error_page()
-
-    return error_page()
+    with open('output.html', 'w', encoding="utf-8") as c_output:
+        c_output.write(Template(course).render(avg_marks=sum(c_marks)/len(c_marks), max_marks=max(c_marks)))
 
 if __name__ == '__main__':
-    app.run()
+    if sys.argv[1] == '-s':
+        student_page(int(sys.argv[2]))
+    elif sys.argv[1] == '-c':
+        course_page(int(sys.argv[2]))
+    else:
+        error_page()
---
+++ week_4/app.py
@@ -0,0 +1,74 @@
+import os
+from flask import Flask, render_template, request
+import matplotlib.pyplot as plt
+
+app = Flask(__name__)
+
+data = []
+with open('data.csv', 'r', encoding="utf-8") as data_file:
+    data_file.readline()
+    for i in data_file.readlines():
+        data.append(list(map(int, i.strip('\n').split(','))))
+
+def error_page():
+    return render_template("error.html")
+
+def student_page(s_id):
+    if s_id not in [i[0] for i in data]:
+        return error_page()
+
+    s_data = []
+    tot_marks = 0
+    for s in data:
+        if s[0] == s_id:
+            s_data.append(s)
+            tot_marks += s[2]
+
+    return render_template("student.html", s_data=s_data, tot_marks=tot_marks)
+
+def course_page(c_id):
+    if c_id not in[i[1] for i in data]:
+        return error_page()
+
+    c_marks = []
+    for c in data:
+        if c_id == c[1]:
+            c_marks.append(c[2])
+
+    path = "./static"
+    if not os.path.exists(path):
+        os.mkdir(path)
+    plt.hist(c_marks)
+    plt.xlabel('Marks')
+    plt.ylabel('Frequency')
+    plt.savefig('./static/hist_of_marks.png')
+
+    return render_template("course.html", avg_marks=sum(c_marks)/len(c_marks), max_marks=max(c_marks))
+
+@app.route('/', methods=["GET", "POST"])
+def main():
+    if request.method == "GET":
+        return render_template('index.html')
+    elif request.method == "POST":
+        id_category = request.form.get("ID")
+        id_value = request.form.get("id_value")
+
+        if not id_category or not id_value:
+            return error_page()
+
+        try:
+            id_value = int(id_value)
+        except ValueError:
+            return error_page()
+
+        if id_category == "student_id":
+            return student_page(id_value)
+        elif id_category == "course_id":
+            return course_page(id_value)
+        else:
+            return error_page()
+
+    return error_page()
+
+if __name__ == '__main__':
+    app.run()
