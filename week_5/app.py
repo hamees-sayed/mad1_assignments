297a4d9bab171f9dd4107b4c8b32c4c1eaa547e3
Hamees Sayed <98336593+hamees-sayed@users.noreply.github.com>
2023-10-26 19:10:11 +0000

    passing test cases

--- week_5/app.py
+++ week_5/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
